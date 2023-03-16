c827d98ba8615b70ffa9159feec39528449ae35a
jwaes <jaco.waes@gmail.com>
2023-03-16 14:21:29 +0000

    docs

--- models/product_product.py
+++ models/product_product.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from os.path import splitext
 
 
 class ProductProduct(models.Model):
@@ -12,7 +13,6 @@ class ProductProduct(models.Model):
         product_website_share = self.env.user.company_id.product_document_website_share
 
         for record in self:
-            blah = 1
             product_documents = self.env['documents.document'].sudo().search([('res_model', '=', self._name), ('res_id', '=', self.id), ('type', '=', 'binary'), ]).filtered(lambda doc: product_website_tag in doc.tag_ids)
             product_template_documents = self.env['documents.document'].sudo().search([('res_model', '=', self.product_tmpl_id._name), ('res_id', '=', self.product_tmpl_id.id), ('type', '=', 'binary'), ]).filtered(lambda doc: product_website_tag in doc.tag_ids)
             all_docs = product_documents | product_template_documents
@@ -20,3 +20,24 @@ class ProductProduct(models.Model):
 
             # for record in self:
             # record.full_url = "%s/document/share/%s/%s" % (record.get_base_url(), record.id, record.access_token)
+
+    def _get_product_website_document_array(self):
+        self.ensure_one()
+
+        documents = []
+        for doc in self.product_document_website_ids:
+            ext = splitext(doc.name)[1].replace(".","").lower()
+            documents.append({
+                'id': doc.id,
+                'name': doc.name,
+                'file_extension': ext,
+            })
+        return documents
+
+    def _has_product_website_documents(self):
+        self.ensure_one()
+        return len(self.product_document_website_ids) > 0
+
+    # def _get_product_document_website_share(self):
+    #     return self.env.user.company_id.product_document_website_share
+
--- models/product_template.py
+++ models/product_template.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+from os.path import splitext
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
@@ -7,4 +7,25 @@ class ProductTemplate(models.Model):
     def product_website_documents(self):
         product_website_tag = self.env.user.company_id.product_document_website_tag
         product_template_documents = self.env['documents.document'].search([('res_model', '=', self._name), ('res_id', '=', self.id), ('type', '=', 'binary'), ]).filtered(lambda doc: product_website_tag in doc.tag_ids)
-        return product_template_documents    
+        return product_template_documents
+
+
+    def _get_product_website_document_array(self):
+        self.ensure_one()
+
+        documents = []
+        for doc in self.product_website_documents():
+            ext = splitext(doc.name)[1].replace(".","").lower()
+            documents.append({
+                'id': doc.id,
+                'name': doc.name,
+                'file_extension': ext,
+            })
+        return documents
+
+    def _has_product_website_documents(self):
+        self.ensure_one()
+        return len(self.product_website_documents()) > 0
+
+    def _get_product_document_website_share(self):
+        return self.env.user.company_id.product_document_website_share        
