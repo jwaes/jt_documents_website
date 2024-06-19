317899241ed36cdf490d84f254fb73dd9a710c4f
jwaes <jaco.waes@gmail.com>
2024-06-19 15:19:19 +0000

    fd

--- static/src/js/variant_mixin.js
+++ static/src/js/variant_mixin.js
@@ -1,27 +1,8 @@
-odoo.define('jt_product_documents_website.VariantMixin', function (require) {
-    'use strict';
-    
-const {Markup} = require('web.utils');
-var VariantMixin = require('sale.VariantMixin');
-var publicWidget = require('web.public.widget');
+/** @odoo-module **/
 
-require('website_sale.website_sale');
+import VariantMixin from "@website_sale/js/variant_mixin";
 
-/**
- * Addition to the variant_mixin._onChangeCombination
- *
- * This will prevent the user from selecting a quantity that is not available in the
- * stock for that product.
- *
- * It will also display various info/warning messages regarding the select product's stock.
- *
- * This behavior is only applied for the web shop (and not on the SO form)
- * and only for the main product.
- *
- * @param {MouseEvent} ev
- * @param {$.Element} $parent
- * @param {Array} combination
- */
+const originalOnChangeCombination = VariantMixin._onChangeCombination;
 VariantMixin._onChangeCombinationProductDocuments = function (ev, $parent, combination) {
     let product_id = 0;
     // needed for list view of variants
@@ -62,20 +43,7 @@ VariantMixin._onChangeCombinationProductDocuments = function (ev, $parent, combi
         }
 
     });
+    originalOnChangeCombination.apply(this, [ev, $parent, combination]);
 };
 
-publicWidget.registry.WebsiteSale.include({
-    /**
-     * Adds the product properties updating to the regular _onChangeCombination method
-     * @override
-     */
-    _onChangeCombination: function () {
-        this._super.apply(this, arguments);
-        VariantMixin._onChangeCombinationProductDocuments.apply(this, arguments);
-    },
-
-});
-
-return VariantMixin;
-
-});
+export default VariantMixin;
